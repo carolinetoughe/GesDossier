6621cce43a0d0552e6d87b2c9ea93250be5cad65
carolinetoughe <55771644+carolinetoughe@users.noreply.github.com>
2019-12-02 23:07:43 +0000

    memoire6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,9 +9,9 @@
   <meta name="keywords" content="free website templates, free bootstrap themes, free template, free bootstrap, free website template">
 
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Raleway|Candal">
-  <link rel="stylesheet" type="text/css" href="{{ asset('design/css/font-awesome.min.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('design/css/bootstrap.min.css') }}">
-  <link rel="stylesheet" type="text/css" href="{{ asset('design/css/style.css') }}">
+  <link rel="stylesheet" type="text/css" href="design/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="design/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="design/css/style.css">
   <!-- =======================================================
     Theme Name: Medilab
     Theme URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
